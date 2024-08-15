38a21b459a5aeaad6e78bffc7682d34c8fa1e4da
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-08-15 20:57:27 +0100

    add the select elements

---
+++ script.js
@@ -0,0 +1,14 @@
+// Select elements
+const calcul = document.getElementById("#calcul");
+const input = document.getElementById("#input");
+const reset = document.getElementById("#reset");
+const clear = document.getElementById("#clear");
+const plusoumoins = document.getElementById("#plusoumoins");
+const percentage = document.getElementById("#percentage");
+const divideby = document.getElementById("#divideby");
+const times = document.getElementById("#times");
+const minus = document.getElementById("#minus");
+const plus = document.getElementById("#plus");
+const equal = document.getElementById("#equal");
+const dot = document.getElementById("#dot");
+const digit = document.getElementsByClassName("digit");
